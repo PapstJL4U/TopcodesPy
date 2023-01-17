fa6a54ffd929c16e0e5fa9229426293f40ef8c5f
Jean Amourrouge <jedi-papstjl@gmx.de>
2023-01-17 15:48:16 +0100

    topcode and scanner py "connverted"

--- topcodes/scanner.py
+++ topcodes/scanner.py
@@ -11,70 +11,72 @@ python by PapstJL4U
 """
 from PIL import Image
 import math as math
+from topcode import TopCode
+
+
 class Scanner(object):
     # original image
-    _image:Image.Image = None
+    _image: Image.Image = None
     # Total width of the image
     _width = 0
     # Total height of the image
     _height = 0
     # Holds process binary pixel data
-    _data:list[int|None] = [None]
+    _data: list[int | None] = [None]
     # Binary view of the image
     _preview = None
     # candidate code count
-    _ccount:int = 0 
+    _ccount: int = 0
     # number of candidates tested
-    _tcount:int = 0
+    _tcount: int = 0
     # maximum width of a topcode unit in pixel
-    _maxu:int = 80
+    _maxu: int = 80
 
     def __init__(self):
         pass
 
-    def scan_by_filename(self,filename:str=None)->list:
+    def scan_by_filename(self, filename: str = None) -> list:
         with Image.open(filename) as im:
             return self.scan_image(im)
 
-    def scan_image(self,image:Image.Image)->list:
+    def scan_image(self, image: Image.Image) -> list:
         """Scan the given image and return a list of all topcodes"""
         self._image = image
         self._preview = None
         self._width = image.width
         self._height = image.height
         self._data = list(image.convert("RGB").getdata())
-        
+
         self._threshold()
         return self._findCode()
 
-    
-    def scan_rgb_data(self, rgb:list[int], width:int, height:int)->list:
+    def scan_rgb_data(self, rgb: list[int], width: int, height: int) -> list:
         self._width = width
         self._height = height
         self._data = rgb
         self._preview = None
-        #unsure
+        # unsure
         self._image = Image.fromarray(rgb, mode="RGB")
-        
+
         self._threshold()
         return self._findCode()
 
     @property
-    def image(self)->Image.Image:
+    def image(self) -> Image.Image:
         """Returns the original, unaltered image"""
         return self._image
-    
+
     @property
-    def imageW(self)->int:
+    def imageW(self) -> int:
         """Returns the width of the current image"""
         return self._width
-    
+
     @property
-    def imageH(self)->int:
+    def imageH(self) -> int:
         """returns the height of the current image"""
         return self._height
 
-    def setMaxCodeDiameter(self, diameter:int = 0):
+    def setMaxCodeDiameter(self, diameter: int = 0):
         """
         Sets the maximum allowable diameter (in pixels) for a TopCode
         identified by the scanner.  Setting this to a reasonable value for
@@ -85,103 +87,104 @@ class Scanner(object):
         setting the maximum diameter too low will prevent valid codes from
         being recognized.  The default value is 640 pixels.
         """
-        f:float = diameter / 8.0
+        f: float = diameter / 8.0
         self._maxu = (int)(math.ceil(f))
 
     @property
-    def ccount(self)->int:
+    def ccount(self) -> int:
         """Returns the number of candidate topcodes found during a scan"""
         return self._ccount
 
     @property
-    def tcount(self)->int:
+    def tcount(self) -> int:
         """returns the number of topcodes tested during the scan"""
         return self._tcount
 
-    def getBW(self, x:int, y:int)->int:
+    def getBW(self, x: int, y: int) -> int:
         """Binary (threshold black/white) value for pixel (x,y)"""
-        pixel = self._data[y *self._width + x]
+        pixel = self._data[y * self._width + x]
         return (pixel >> 24) & 0x01
 
-    def getSample3x3(self, x:int, y:int)->int:
+    def getSample3x3(self, x: int, y: int) -> int:
         """
         Average of thresholded pixels in a 3x3 region around (x,y).
-        Returned value is between 0 (black) and 255 (white).   
+        Returned value is between 0 (black) and 255 (white).
         """
 
-        if( x < 1 
-            or x > (self._width-2) 
-            or y < 1 
-            or y > (self._height-2)):
+        if x < 1 or x > (self._width - 2) or y < 1 or y > (self._height - 2):
             return 0
-        pixel:int = 0 
-        sum:int = 0
-        for j in range(y-1,y+1, 1):
-            for i in range (x-1, x+1, 1):
-                pixel = self.data[j*self._width+i]
-                if ((pixel & 0x01000000) > 0):
-                    sum+=0xff
-        
+        pixel: int = 0
+        sum: int = 0
+        for j in range(y - 1, y + 1, 1):
+            for i in range(x - 1, x + 1, 1):
+                pixel = self.data[j * self._width + i]
+                if (pixel & 0x01000000) > 0:
+                    sum += 0xFF
+
         return sum / 9
 
-    def getBW3x3(self, x:int, y:int)->int:
+    def getBW3x3(self, x: int, y: int) -> int:
         """
         Average of thresholded pixels in a 3x3 region around (x,y).
-        Returned value is either 0 (black) or 1 (white).    
+        Returned value is either 0 (black) or 1 (white).
         """
-        if( x < 1 
-            or x > (self._width-2) 
-            or y < 1 
-            or y > (self._height-2)):
+        if x < 1 or x > (self._width - 2) or y < 1 or y > (self._height - 2):
             return 0
-        
-        pixel:int = 0
-        sum:int = 0
-        for j in range(y-1,y+1, 1):
-            for i in range (x-1, x+1, 1):
-                pixel = self.data[j*self._width+i]
-                sum+=((pixel >> 24) & 0x01)
+
+        pixel: int = 0
+        sum: int = 0
+        for j in range(y - 1, y + 1, 1):
+            for i in range(x - 1, x + 1, 1):
+                pixel = self.data[j * self._width + i]
+                sum += (pixel >> 24) & 0x01
         if sum >= 5:
             return 1
         else:
             return 0
-    
-    def threshold(self)->None:
+
+    def threshold(self) -> None:
         """
         Perform Wellner adaptive thresholding to produce binary pixel
         data.  Also mark candidate spotcode locations.
-        
-        "Adaptive Thresholding for the DigitalDesk"   
-        EuroPARC Technical Report EPC-93-110
-            """
-        pixel:int = 0
-        r:int; g:int; b:int; a:int
-        threshold:int = 128
-        sum:int = 128
-        s:int = 30
-        k:int = 0
-        b1:int; w1:int; b2:int; level:int; dk:int
 
-        self._ccount = 0;
+        "Adaptive Thresholding for the DigitalDesk"
+        EuroPARC Technical Report EPC-93-110
+        """
+        pixel: int = 0
+        r: int
+        g: int
+        b: int
+        a: int
+        threshold: int = 128
+        sum: int = 128
+        s: int = 30
+        k: int = 0
+        b1: int
+        w1: int
+        b2: int
+        level: int
+        dk: int
+
+        self._ccount = 0
 
         for j in range(self._height):
-            level, w1, b2, level, dk = 0,0,0,0,0
+            level, w1, b2, level, dk = 0, 0, 0, 0, 0
             """
             Process rows back and forth 
             (alternating left-2-right, right-2-left)
             """
-            k = 0 if (j % 2 == 0 ) else (self.width-1)
-            k += (j*self.width)
+            k = 0 if (j % 2 == 0) else (self.width - 1)
+            k += j * self.width
 
             for i in range(self._width):
                 """
                 Calculate pixen intensity (0-255)
                 """
                 pixel = self._data[k]
-                r = (pixel >> 16) & 0xff
-                g = (pixel >> 8) & 0xff
-                r = (pixel) & 0xff
-                a = (r + g +b) / 3
+                r = (pixel >> 16) & 0xFF
+                g = (pixel >> 8) & 0xFF
+                r = (pixel) & 0xFF
+                a = (r + g + b) / 3
 
                 """
                 Calculate sum as an approximate sum 
@@ -193,7 +196,7 @@ class Scanner(object):
                 Cimpare the average sum to current
                 pixel to decide black or white
                 """
-                f:float = 0.85
+                f: float = 0.85
                 f = 0.975
                 a = 0 if (a < threshold * f) else 1
 
@@ -202,63 +205,157 @@ class Scanner(object):
                 the alpha channel, and the running sum
                 for this pixel in the rgb channels
                 """
-                self._data[k] = (a << 24) + (sum & 0xffffff)
+                self._data[k] = (a << 24) + (sum & 0xFFFFFF)
 
-                #on a white region, no black pixels
+                # on a white region, no black pixels
                 if level == 0:
-                    #first black pixel encountered
+                    # first black pixel encountered
                     if a == 0:
                         level = 1
                         b1 = 1
-                        w1 = 0 
+                        w1 = 0
                         b2 = 0
-                #on first black region
-                elif level == 1: 
-                    if a==0:
-                        b1+=1
+                # on first black region
+                elif level == 1:
+                    if a == 0:
+                        b1 += 1
                     else:
                         level = 2
                         w1 = 1
-                #on second white region (bullseye of a code?)
+                # on second white region (bullseye of a code?)
                 elif level == 2:
                     if a == 0:
                         level = 3
                         b2 = 1
                     else:
-                        w1+=1
-                
-                #on second black region
-                elif level == 3:
-                    if a==0:
-                        b2+=1
-                    #this could be a top code
-                    else:
-                        mask:int=0
+                        w1 += 1
 
-                        if( b1 >= 2 
+                # on second black region
+                elif level == 3:
+                    if a == 0:
+                        b2 += 1
+                    # this could be a top code
+                    else:
+                        mask: int = 0
+
+                        if (
+                            b1 >= 2
                             and b2 >= 22
                             and b1 <= self._maxu
                             and w1 <= (self._maxu + self._maxu)
-                            and math.abs(b1+b2-w1) <= (b1+b2)
-                            and math.abs(b1+b2-w1) <= w1
-                            and math.abs(b1-b2) <= b1
-                            and math.abs(b1-b2) <= b2
+                            and math.abs(b1 + b2 - w1) <= (b1 + b2)
+                            and math.abs(b1 + b2 - w1) <= w1
+                            and math.abs(b1 - b2) <= b1
+                            and math.abs(b1 - b2) <= b2
                         ):
                             mask = 0x2000000
 
-                            dk = 1 + b1 + w1/2
-                            if(j%2==0):
+                            dk = 1 + b1 + w1 / 2
+                            if j % 2 == 0:
                                 dk = k - dk
                             else:
                                 dk = k + dk
-                            
+
                             self._data[dk - 1] |= mask
                             self._data[dk] >= mask
-                            self._data[dk+1] |= mask
-                            self._ccount += 3 # count candidate codes
-                        
+                            self._data[dk + 1] |= mask
+                            self._ccount += 3  # count candidate codes
+
                         b1 = b2
                         w1 = 1
                         b2 = 0
                         level = 2
             k += 1 if (j % 2 == 0) else -1
+
+    def findCodes(self) -> list[TopCode]:
+        self._tcount = 0
+        spots: list[TopCode] = []
+        spot: TopCode = TopCode()
+        k: int = self.width * 2
+        for j in range(self.height - 2):
+            for i in range(self.width):
+                if (self._datadata[k] & 0x2000000) > 0:
+                    if (
+                        (self._data[k - 1] & 0x2000000) > 0
+                        and (self._data[k + 1] & 0x2000000) > 0
+                        and (self._data[k - self.width] & 0x2000000) > 0
+                        and (self._data[k + self.width] & 0x2000000) > 0
+                    ):
+                        if not self.overlaps(spot, i, j):
+                            self._tcount += 1
+                            spot.decode(self, i, j)
+                            if spot.isValid():
+                                spots.add(spot)
+                                spot = TopCode()
+                k += 1
+        return spots
+
+    def overlaps(self, spots: list[TopCode], x: int, y: int) -> bool:
+        """
+        Returns true if point(x,y) is in an exissting TopCode bullseye
+        """
+        for topcode in spots:
+            if topcode.inBullsEye(x, y):
+                return True
+        return False
+
+    def ydist(self, x: int, y: int, d: int) -> int:
+        """
+        Counts the number of vertical pixels from (x,y)
+        until a color change is perceived
+        """
+        sample: int = 0
+        start: int = self.getBW3x3(x, y)
+
+        j: int = y + d
+        while j > 1 and j < (self.height - 1):
+            sample = self.getBW3x3(x, j)
+            if start + sample == 1:
+                value = (j - y) if d > 0 else (y - j)
+                return value
+            j += d
+        return -1
+
+    def xdist(self, x: int, y: int, d: int) -> int:
+        """
+        Counts the number of horizontal pixel from (x,y)
+        until a color change is perceived
+        Return = -1: error
+
+        """
+        sample: int = 0
+        start: int = self.getBW3x3(x, y)
+
+        i: int = x + d
+        while i > 1 and i < (self._width - 1):
+            sample = self.getBW3x3(i, y)
+            if start + sample == 1:
+                value = (i - x) if d > 0 else (x - i)
+                return value
+        return -1
+
+    def getPreview(self) -> Image:
+        """
+        For debugging purposes, create a black and white image
+        that shows the result of adaptive thresholding
+        """
+        if self._preview != None:
+            return self._preview
+        self._preview = Image.new(mode="RGB", size=(self.width, self.height))
+
+        pixel: int = 0
+        k: int = 0
+        for j in range(self.height):
+            for i in range(self.width):
+                pixel = self._data[k + 1] >> 24
+                if pixel == 0:
+                    pixel == 0xFF000000
+                elif pixel == 1:
+                    pixel == 0xFFFFFFFF
+                elif pixel == 3:
+                    pixel == 0xFF00FF00
+                elif pixel == 7:
+                    pixel == 0xFFFF0000
+                self._preview.setRGB(i, j, pixel)
+
+        return self._preview
