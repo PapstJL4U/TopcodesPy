bd56ae139f2790ab592633bc0d049cc9f8490a23
PapstJL4U <PapstJL4U@users.noreply.github.com>
2023-01-18 16:12:16 +0100

    basic layout, no function

--- topcodes/debugwindow.py
+++ topcodes/debugwindow.py
@@ -1,2 +1,43 @@
 """
 """
+import PySimpleGUI as sg
+"""
+Layout
+"""
+sg.theme('Default1') 
+
+bottom = sg.Column([[
+    sg.Text("Codes found:")], 
+    [sg.Multiline(
+                default_text="", key="-output-", size=(80, 5), autoscroll=True
+            )
+]])
+
+mid = sg.Column([[
+    sg.Button("Highlight Codes", key="-highlight-"),
+    sg.Button("Show Threshold", key="-treshold-"),
+    sg.Button("Load Image", key="-load-")
+]])
+
+top = sg.Column([[
+    sg.Image(source = r"topcodes/test_img/tops.png", size=(700,700), key="-image-")
+]])
+
+layout = [[top],[mid], [bottom]]
+
+window = sg.Window("TopCode-Debug", layout, finalize=False)
+"""
+Functions
+"""
+
+
+while True:
+    event, values = window.read()
+    if (
+        event == sg.WIN_CLOSED or event == "Cancel"
+    ):  # if user closes window or clicks cancel
+        break
+    if event == "-close-":
+        pass
+
+window.close()
